b1320447fac82194273cd072539fb2060b050d38
EmberOctane <emberoctane@Ravalis-MacBook-Pro.local>
2019-12-07 22:07:19 -0800

    xray test code

--- src/zeus/index.js
+++ src/zeus/index.js
@@ -1,7 +1,7 @@
 import {one_sec, msg} from './constants';
 import cron from 'node-cron';
 
-cron.schedule('*/5 * * * * *', ()=>{
+cron.schedule('*/5 * * * * *', () => {
     console.log(' hello from the watch mode::', msg);
 });
 
